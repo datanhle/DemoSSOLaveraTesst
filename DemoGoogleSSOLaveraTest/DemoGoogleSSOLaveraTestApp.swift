75652c934905b225bfaf25182a25e4975d9a6ba8
datla <datleanhbkdn@gmail.com>
2025-08-08 09:11:54 +0700

    Initial Commit

---
+++ DemoGoogleSSOLaveraTest/DemoGoogleSSOLaveraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGoogleSSOLaveraTestApp.swift
+//  DemoGoogleSSOLaveraTest
+//
+//  Created by DatLeAnh on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoGoogleSSOLaveraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
